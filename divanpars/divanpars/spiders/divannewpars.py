4e6236f43978acbcde813ebde0a8800c6dc9062b
Skeyling1 <skeyling1@gmail.com>
2024-07-13 15:45:17 +0300

    Update divannewpars.py

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -1,10 +1,14 @@
+# написать spider для нахождения всех источников освещения с сайта divan.ru
+# # Нужно взять название источника освещения, цену и ссылку
+# # *Можно попробовать сделать это на другом сайте с продажей источников освещения
+
 import scrapy
 
 
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
-    allowed_domains = ["divan.ru"]
-    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
         pass
